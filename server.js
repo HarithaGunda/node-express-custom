fa7b1653cbd09e9bcd88c7ae9879ed6d5fd1e562
Haritha Gunda <gunda.haritha@gmail.com>
2020-03-14 16:51:35 -0400

    formatted

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const morgan = require('morgan');
+const express=require('express');
+const morgan=require('morgan');
 const bodyParser=require('body-parser');
 const campsiteRouter=require('./routes/campsiteRouter');
 const promotionRouter=require('./routes/promotionRouter');
 const partnerRouter=require('./routes/partnerRouter');
-const hostname = 'localhost';
-const port = 3000;
+const hostname='localhost';
+const port=3000;
 
-const app = express();
+const app=express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
@@ -18,8 +18,8 @@ app.use('/partners', partnerRouter);
 app.use(express.static(__dirname+'/public'));
 
 app.use((req, res) => {
-   // console.log(req.headers);
-    res.statusCode = 200;
+    // console.log(req.headers);
+    res.statusCode=200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
 });
