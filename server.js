182c05c89db02484cd345311bac67247684fa50f
Haritha Gunda <gunda.haritha@gmail.com>
2020-03-18 17:30:56 -0400

    Revert "Intial project setup for express server along with 4 routers added"
    This reverts commit ce91a8d5e9cdd683f0f2e3467eab69249c5dc016.

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express=require('express');
 const morgan=require('morgan');
 const bodyParser=require('body-parser');
-const articlesRouter=require('./routes/articlesRouter');
-const articlesjavaRouter=require('./routes/articlesjavaRouter');
-const articleshtmlRouter=require('./routes/articleshtmlRouter');
-const commentsRouter=require('./routes/commentsRouter');
+const campsiteRouter=require('./routes/campsiteRouter');
+const promotionRouter=require('./routes/promotionRouter');
+const partnerRouter=require('./routes/partnerRouter');
 const hostname='localhost';
 const port=3000;
 
@@ -12,10 +11,9 @@ const app=express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
-app.use('/articles', articlesRouter);
-app.use('/articlesjava', articlesjavaRouter);
-app.use('/articleshtml', articleshtmlRouter);
-app.use('/comments', commentsRouter);
+app.use('/campsites', campsiteRouter);
+app.use('/promotions', promotionRouter);
+app.use('/partners', partnerRouter);
 
 app.use(express.static(__dirname+'/public'));
 
@@ -23,7 +21,7 @@ app.use((req, res) => {
     // console.log(req.headers);
     res.statusCode=200;
     res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>This is an Express Server for the Learn and Share Blog</h1></body></html>');
+    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
 });
 
 
